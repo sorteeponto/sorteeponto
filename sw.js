01d3cf4cc489781cb073208d47acb4b4fca438a7
sorteeponto <sorteeponto@gmail.com>
2026-05-28 21:56:44 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'sorte-ponto-v2';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Não cachear requests do Firebase
+  if (e.request.url.includes('firebaseio.com') || 
+      e.request.url.includes('googleapis.com') ||
+      e.request.url.includes('gstatic.com')) {
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
